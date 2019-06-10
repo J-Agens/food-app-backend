320b3504324155212ac61c3f702a57f3cdc17d7b
J-Agens <johntagens@gmail.com>
2019-06-10 13:38:31 -0400

    fixing bugs

--- db/seeds.rb
+++ db/seeds.rb
@@ -70,7 +70,7 @@ recipe_ings.each do |item|
 
 end
 
-10.times do
+20.times do
   table = Recipe.all.sample
   Order.create(item_name: recipes.sample, served: false, price: rand(5..20), user_id: rand(User.first.id..User.last.id), table_id: table.id)
 end
