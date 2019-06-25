2b15640b2dca369297f3008afdd40c8f9e3fd130
J-Agens <johntagens@gmail.com>
2019-06-25 18:24:56 -0400

    .

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,7 +35,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   config.action_cable.mount_path = nil
   config.action_cable.url = 'wss://cryptic-scrubland-43079.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = [ 'https://cryptic-scrubland-43079.herokuapp.com/', /https:\/\/cryptic-scrubland-43079.herokuapp.*/ ]
+  config.action_cable.allowed_request_origins = [ 'https://floating-spire-37575.herokuapp.com/', /https:\/\/floating-spire-37575.herokuapp.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
