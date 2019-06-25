e5322c711bf57072969596853ff04e5de601876c
J-Agens <johntagens@gmail.com>
2019-06-25 18:18:57 -0400

    moved routes around

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
   resources :tables, only:[:index, :show]
-  resources :users, only:[:index, :show] 
+  resources :users, only:[:index, :show]
   resources :orders, only:[:create, :update, :destroy]
   resources :cook_sessions, only:[:index, :show, :create, :update, :destroy]
   resources :recipes, only:[:index]
   resources :ingredients, only:[:index]
   resources :pots, only:[:index]
-  mount ActionCable.server => '/cable'
+
   post '/signup', to: 'users#create'
   post '/login', to: 'auth#login'
   get '/auto_login', to: 'auth#auto_login'
+  mount ActionCable.server => '/cable' # moved this below login
   # get '/erase_orders', to: 'orders#erase_orders' <-- used in development
   post '/pay_bill', to: 'users#pay_bill'
   post '/cancel', to: 'orders#cancel'
